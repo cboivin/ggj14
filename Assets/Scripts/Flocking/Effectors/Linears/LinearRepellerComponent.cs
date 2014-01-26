ee82650febc3257046397c28fd37b1de625b75ce
mcklane <lucascurci.register@gmail.com>
2014-01-26 01:46:20 +0100

    linear repellers

--- Assets/Scripts/Flocking/Effectors/Linears/LinearRepellerComponent.cs
+++ Assets/Scripts/Flocking/Effectors/Linears/LinearRepellerComponent.cs
@@ -7,18 +7,18 @@ namespace GameJam.Boids {
 	public class LinearRepellerComponent : LinearEffector {
 	
 		public float intensity;
-		private float sqrEffectDistance;
+//		private float sqrEffectDistance;
 
 		protected override void OnUpdate() {
-			this.sqrEffectDistance = this.effectDistance*this.effectDistance;	
+//			this.sqrEffectDistance = this.effectDistance*this.effectDistance;	
 		}
 
 		public override void ApplyEffect (Boid other) {
 			Vector3 direction = this.ComputeDistance(other);
-			if ( direction.sqrMagnitude > this.sqrEffectDistance ) {
+			if ( !this.ComputeSide(other) ) {
 				return;
-			}
-			other.repulsionVel += direction.normalized * this.intensity / direction.sqrMagnitude;
+			};
+			other.repulsionVel += direction.normalized * this.intensity * direction.sqrMagnitude;
 			other.repulsionEffectors++;
 		}
 
@@ -34,20 +34,6 @@ namespace GameJam.Boids {
 			Gizmos.DrawRay(middle.origin, middle.direction * 50f);
 			middle.direction *= -1f;
 			Gizmos.DrawRay(middle.origin, middle.direction * 50f);
-
-			Gizmos.color = Color.cyan;
-			Ray max = this.linePosition;
-			max.origin = this.myTransform.rotation * Vector3.right * this.effectDistance + this.myTransform.position;
-			Gizmos.DrawRay(max.origin, max.direction * 50f);
-			max.direction *= -1f;
-			Gizmos.DrawRay(max.origin, max.direction * 50f);
-
-			Ray min = this.linePosition;
-			min.origin = this.myTransform.rotation * Vector3.left * this.effectDistance + this.myTransform.position;
-			Gizmos.DrawRay(min.origin,min.direction * 50f);
-			min.direction *= -1f;
-			Gizmos.DrawRay(min.origin, min.direction * 50f);
-
 		}
 
 	}
